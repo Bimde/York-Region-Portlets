d5b9f1faecfbac22a7e9100aeec368b65dd6d4b1
DeSilva <Bimesh.DeSilva@york.ca>
2015-12-22 13:54:29 -0500

    Fixed Facebook Page Plugin customization

--- Facebook Page Plugin Customizer/fbPagePluginCustomizer.js
+++ Facebook Page Plugin Customizer/fbPagePluginCustomizer.js
@@ -1,42 +1,44 @@
-function __SPNS__customizeWidget()
-{
-  var page = document.getElementById("link").value;
+function customizeWidget() {
+  verifyInput();
+  displayWidget();
+}
+
+function verifyInput() {
+  var link = document.getElementById("link").value;
   var width = document.getElementById("width").value;
   var length = document.getElementById("length").value;
-  __SPNS__verifyInput(page, width, length);
-}
-
-function __SPNS__verifyInput(link, width, length)
-{
-  if(link == "") {
+  if(link === "") {
     link = "https://www.facebook.com/YorkRegionGovt";
   }
-  if(width == "" || isNaN(width)) {
+  if(width === "" || isNaN(width)) {
     width = 300;
   }
-  if(length == "" || isNaN(length)) {
+  if(length === "" || isNaN(length)) {
     length = 500;
-  }  
-  __SPNS__createCustomWidget(link, width, length);
+  }
+
+  createCustomWidget(link, width, length);
 }
 
-function __SPNS__createCustomWidget(link, width, length)
-{  
-  // chnage data to specified values
+function createCustomWidget(link, width, length) {
   document.getElementById("fbPage").removeAttribute("data-href");
   document.getElementById("fbPage").setAttribute("data-href", link);
   document.getElementById("fbPage").removeAttribute("data-height");
   document.getElementById("fbPage").setAttribute("data-height", length);
   document.getElementById("fbPage").removeAttribute("data-width");
   document.getElementById("fbPage").setAttribute("data-width", width);
-  
+
   // Make facebook widget visible
+  loadfb(document, 'script', 'facebook-jssdk');
+}
+
+function displayWidget() {
   document.getElementById("facebookWidget").className = "";
   document.getElementById("form").className = "displayNone";
 }
 
-function __SPNS__previewWidget ()
-{
+function previewWidget () {
+  verifyInput();
   if(document.getElementById("facebookWidget").className != "") {
     document.getElementById("facebookWidget").className = "";
   }
@@ -45,10 +47,10 @@ function __SPNS__previewWidget ()
   }
 }
 
-(function(d, s, id) {
+function loadfb(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "http://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
   fjs.parentNode.insertBefore(js, fjs);
-})(document, 'script', 'facebook-jssdk');
+}
