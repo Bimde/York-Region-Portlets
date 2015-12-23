8024e967e828ac09d463f7d7380c7452fa4b97c8
DeSilva <Bimesh.DeSilva@york.ca>
2015-12-23 10:51:11 -0500

    Implemented reloading of widget on dimension change

--- Facebook Page Plugin Customizer/fbPagePluginCustomizer.js
+++ Facebook Page Plugin Customizer/fbPagePluginCustomizer.js
@@ -1,3 +1,6 @@
+// Not 'const' to allow to better browser support
+var __SPNS__MIN_WIDTH = 180, __SPNS__MAX_WIDTH = 500, __SPNS__MIN_HEIGHT = 70;
+
 function customizeWidget() {
   verifyInput();
   displayWidget();
@@ -11,46 +14,62 @@ function verifyInput() {
     link = "https://www.facebook.com/YorkRegionGovt";
   }
   if(width === null || width === "" || isNaN(width)) {
-    width = "100%";
+    width = "300";
   }
   if(length === null || length === "" || isNaN(length)) {
-    length = 500;
+    length = "500";
   }
   createCustomWidget(link, width, length);
+  //loadFB(document, 'script', 'facebook-jssdk');
 }
 
 function createCustomWidget(link, width, height) {
+  // This prevents the container div from becoming larger than the widget
+  if (width > __SPNS__MAX_WIDTH) {
+    width = __SPNS__MAX_WIDTH;
+  }
   document.getElementById("fbPage").removeAttribute("data-href");
   document.getElementById("fbPage").setAttribute("data-href", link);
   document.getElementById("fbPage").removeAttribute("data-height");
-  document.getElementById("fbPage").setAttribute("data-height", height);
   document.getElementById("fbPage").removeAttribute("data-width");
-  document.getElementById("fbPage").setAttribute("data-width", width);
+  // Checks here to adhere to facebook's minimum width / height policies
+  document.getElementById("fbPage").setAttribute("data-height", height < __SPNS__MIN_HEIGHT ? __SPNS__MIN_HEIGHT : height);
+  document.getElementById("fbPage").setAttribute("data-width", width < __SPNS__MIN_WIDTH ? __SPNS__MIN_WIDTH : width);
+
+  document.getElementById("facebookWidget__SPNS__").style.width = width + "px";
+  document.getElementById("facebookWidget__SPNS__").style.height = height + "px";
+
+  console.log(document.getElementById("fb_root"));
   loadFB(document, 'script', 'facebook-jssdk');
 }
 
 function displayWidget() {
   document.getElementById("form").className = "displayNone";
-  document.getElementById("facebookWidget__SPNS__").className = "";
+  document.getElementById("overflow__SPNS__").style.display = "block";
 }
 
-function previewWidget () {
-  verifyInput();
-  if(document.getElementById("facebookWidget__SPNS__").className !== "") {
-    document.getElementById("facebookWidget__SPNS__").className = "";
+function toggleWidget() {
+  console.log(document.getElementById("overflow__SPNS__").style.display);
+  if(document.getElementById("overflow__SPNS__").style.display !== "block") {
+    verifyInput();
+    document.getElementById("overflow__SPNS__").style.display = "block";
     document.getElementById("__SPNS__preview-button").innerHTML = "Close Preview";
+    console.log("entered1: " + document.getElementById("overflow__SPNS__").style.display);
   }
   else {
-    document.getElementById("facebookWidget__SPNS__").className = "displayNone";
+    document.getElementById("overflow__SPNS__").style.display = "none";
     document.getElementById("__SPNS__preview-button").innerHTML = "Preview Widget";
+    console.log("entered2: " + document.getElementById("overflow__SPNS__").style.display);
   }
 }
 
 function loadFB(d, s, id) {
   console.log(d + " , " + s + ", " + id);
   var js, fjs = d.getElementsByTagName(s)[0];
+
+  // Reloads the widget with the new width / height if ti already exists
   if (d.getElementById(id)) {
-    return;
+     FB.XFBML.parse();
   }
   js = d.createElement(s); js.id = id;
   js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
